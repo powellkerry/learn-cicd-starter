12b04b0ceefe61978419a69a2fbd9731d3bae94b
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2025-04-02 13:53:19 -0600

    Fix security issues

--- json.go
+++ json.go
@@ -30,5 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	n, write_err := w.Write(dat)
+	if write_err != nill {
+		log.Printf("Write failed: %v", write_err)
+	}
 }
