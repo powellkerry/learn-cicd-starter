9150808e0ea0d874ff7cc573e09e7ab2ccd38979
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2025-04-02 13:58:57 -0600

    Fix test errors

--- json.go
+++ json.go
@@ -30,8 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	n, write_err := w.Write(dat)
-	if write_err != nill {
+	_, write_err := w.Write(dat)
+	if write_err != nil {
 		log.Printf("Write failed: %v", write_err)
 	}
 }
